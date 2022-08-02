3ef91a1a9d4cdb39ef5cfd764ea07f83212e1ebc
melike <melkedemr@gmail.com>
2022-08-02 12:49:07 +0100

    first commit

--- src/test/java/b22/bookit/BookitEnvTest.java
+++ src/test/java/b22/bookit/BookitEnvTest.java
@@ -15,6 +15,8 @@ public class BookitEnvTest {
             System.out.println(ConfigReader.getProperty("teacher_email"));
             System.out.println(ConfigReader.getProperty("teacher_password"));
             System.out.println(ConfigReader.getProperty("base_path"));
+
+
         }
 
 }
